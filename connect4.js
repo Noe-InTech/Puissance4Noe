4a90fab81098cf41ec2ce0d2d8ae4b63add08611
Noe-InTech <73590086+Noe-InTech@users.noreply.github.com>
2023-02-13 02:57:57 +0100

    Add files via upload

---
+++ connect4.js
@@ -0,0 +1,318 @@
+matriceFixeResultats = [
+  //    A     B     C     D     E     F     G
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "A1", "B1", "C1", "D1", "E1", "F1", "G1", "OU", "OU", "OU", "OU"], // 1
+  ["OU", "OU", "OU", "OU", "OU", "A2", "B2", "C2", "D2", "E2", "F2", "G2", "OU", "OU", "OU", "OU"], // 2
+  ["OU", "OU", "OU", "OU", "OU", "A3", "B3", "C3", "D3", "E3", "F3", "G3", "OU", "OU", "OU", "OU"], // 3
+  ["OU", "OU", "OU", "OU", "OU", "A4", "B4", "C4", "D4", "E4", "F4", "G4", "OU", "OU", "OU", "OU"], // 4
+  ["OU", "OU", "OU", "OU", "OU", "A5", "B5", "C5", "D5", "E5", "F5", "G5", "OU", "OU", "OU", "OU"], // 5
+  ["OU", "OU", "OU", "OU", "OU", "A6", "B6", "C6", "D6", "E6", "F6", "G6", "OU", "OU", "OU", "OU"], // 6
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+
+];
+
+matrice = [
+  //    A     B     C     D     E     F     G
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "A1", "B1", "C1", "D1", "E1", "F1", "G1", "OU", "OU", "OU", "OU"], // 1
+  ["OU", "OU", "OU", "OU", "OU", "A2", "B2", "C2", "D2", "E2", "F2", "G2", "OU", "OU", "OU", "OU"], // 2
+  ["OU", "OU", "OU", "OU", "OU", "A3", "B3", "C3", "D3", "E3", "F3", "G3", "OU", "OU", "OU", "OU"], // 3
+  ["OU", "OU", "OU", "OU", "OU", "A4", "B4", "C4", "D4", "E4", "F4", "G4", "OU", "OU", "OU", "OU"], // 4
+  ["OU", "OU", "OU", "OU", "OU", "A5", "B5", "C5", "D5", "E5", "F5", "G5", "OU", "OU", "OU", "OU"], // 5
+  ["OU", "OU", "OU", "OU", "OU", "A6", "B6", "C6", "D6", "E6", "F6", "G6", "OU", "OU", "OU", "OU"], // 6
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+  ["OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU", "OU"], // Ou
+
+];
+var player = "01";
+var winner = false;
+document.getElementById("couleurText").className = "joueurRouge";
+function resultat(a, b, c, d, e, f, g, h) {
+
+  console.log(matriceFixeResultats[a][b] + " " + matriceFixeResultats[c][d] + " " + matriceFixeResultats[e][f] + " " + matriceFixeResultats[g][h]);
+  document.getElementById(matriceFixeResultats[a][b]).setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#green");
+  document.getElementById(matriceFixeResultats[c][d]).setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#green");
+  document.getElementById(matriceFixeResultats[e][f]).setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#green");
+  document.getElementById(matriceFixeResultats[g][h]).setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#green");
+winner = true;
+
+}
+
+function textePage() {
+  if (player == "02") {
+    couleur = "  rouge";
+    classcolor = "joueurRouge"
+    gagnant = "jaune"; 
+    gagnantColor = "joueurJaune"
+      
+     
+  } else {
+    couleur = "  jaune";
+    classcolor = "joueurJaune"
+    gagnant = "rouge"; 
+    gagnantColor = "joueurRouge"
+  }
+  document.getElementById("couleurText").innerHTML = " " +couleur;
+  document.getElementById("couleurText").className = classcolor;
+
+  if (winner == true) {
+    document.getElementById("couleurText").innerHTML =   gagnant;
+    document.getElementById("couleurText").className = gagnantColor;
+
+    document.getElementById("winnerText").innerHTML = "Le gagnant est le ";
+    //empeche le clic
+    document.getElementById("svg").style.pointerEvents = "none";
+  }
+}
+
+function checker(j, i, player) {
+
+
+
+  if (matrice[i][j] == player &&
+    matrice[i - 3][j] == player &&
+    matrice[i - 1][j] == player &&
+    matrice[i - 2][j] == player) {
+
+    resultat(i, j, i - 3, j, i - 1, j, i - 2, j);
+    textePage();
+  };
+
+
+
+  if (matrice[i][j] == player &&
+    matrice[i + 1][j] == player &&
+    matrice[i - 2][j] == player &&
+    matrice[i - 1][j] == player) {
+
+    resultat(i, j, i + 1, j, i - 2, j, i - 1, j);
+    textePage();
+
+  };
+
+
+  if (matrice[i][j] == player &&
+    matrice[i - 1][j] == player &&
+    matrice[i + 1][j] == player &&
+    matrice[i + 2][j] == player) {
+
+    resultat(i, j, i - 1, j, i + 1, j, i + 2, j);
+    textePage();
+  };
+
+  /////////////////////////
+  console.log(matrice[i][j] + " " +
+    matrice[i + 1][j] + " " +
+    matrice[i + 2][j] + " " +
+    matrice[i + 3][j])
+  /////////////////////////
+  if (matrice[i][j] == player &&
+    matrice[i + 1][j] == player &&
+    matrice[i + 2][j] == player &&
+    matrice[i + 3][j] == player) {
+
+    resultat(i, j, i + 1, j, i + 2, j, i + 3, j);
+    textePage();
+  };
+
+
+
+
+  if (matrice[i][j] == player &&
+    matrice[i][j + 1] == player &&
+    matrice[i][j + 2] == player &&
+    matrice[i][j + 3] == player) {
+
+    resultat(i, j, i, j + 1, i, j + 2, i, j + 3);
+
+    textePage();
+  };
+
+
+
+  if (matrice[i][j] == player &&
+    matrice[i][j - 1] == player &&
+    matrice[i][j + 1] == player &&
+    matrice[i][j + 2] == player) {
+    resultat(i, j, i, j - 1, i, j + 1, i, j + 2);
+
+
+    textePage();
+  };
+
+
+  if (matrice[i][j] == player &&
+    matrice[i][j + 1] == player &&
+    matrice[i][j - 1] == player &&
+    matrice[i][j - 2] == player) {
+    resultat(i, j, i, j + 1, i, j - 1, i, j - 2);
+
+    textePage();
+  };
+
+
+
+
+  if (matrice[i][j] == player &&
+    matrice[i][j - 1] == player &&
+    matrice[i][j - 2] == player &&
+    matrice[i][j - 3] == player) {
+    resultat(i, j, i, j - 1, i, j - 2, i, j - 3);
+
+    textePage();
+  };
+
+
+
+  if (matrice[i][j] == player &&
+    matrice[i + 1][j - 1] == player &&
+    matrice[i + 2][j - 2] == player &&
+    matrice[i + 3][j - 3] == player) {
+    resultat(i, j, i + 1, j - 1, i + 2, j - 2, i + 3, j - 3);
+
+    textePage();
+  };
+
+
+  if (matrice[i][j] == player &&
+    matrice[i - 1][j + 1] == player &&
+    matrice[i + 1][j - 1] == player &&
+    matrice[i + 2][j - 2] == player) {
+
+    resultat(i, j, i - 1, j + 1, i + 1, j - 1, i + 2, j - 2);
+    textePage();
+  };
+
+
+  if (matrice[i][j] == player &&
+    matrice[i - 2][j + 2] == player &&
+    matrice[i - 1][j + 1] == player &&
+    matrice[i + 1][j - 1] == player) {
+    resultat(i, j, i - 2, j + 2, i - 1, j + 1, i + 1, j - 1);
+    textePage();
+
+  };
+
+
+  if (matrice[i][j] == player &&
+    matrice[i - 1][j + 1] == player &&
+    matrice[i - 2][j + 2] == player &&
+    matrice[i - 3][j + 3] == player) {
+    resultat(i, j, i - 1, j + 1, i - 2, j + 2, i - 3, j + 3);
+
+    textePage();
+  };
+
+
+
+  if (matrice[i][j] == player &&
+    matrice[i + 1][j + 1] == player &&
+    matrice[i + 2][j + 2] == player &&
+    matrice[i + 3][j + 3] == player) {
+
+    resultat(i, j, i + 1, j + 1, i + 2, j + 2, i + 3, j + 3);
+    textePage();
+  };
+
+
+  if (matrice[i][j] == player &&
+    matrice[i - 1][j - 1] == player &&
+    matrice[i + 1][j + 1] == player &&
+    matrice[i + 2][j + 2] == player) {
+    resultat(i, j, i - 1, j - 1, i + 1, j + 1, i + 2, j + 2);
+
+    textePage();
+  };
+
+
+  if (matrice[i][j] == player &&
+    matrice[i - 2][j - 2] == player &&
+    matrice[i - 1][j - 1] == player &&
+    matrice[i + 1][j + 1] == player) {
+    resultat(i, j, i - 2, j - 2, i - 1, j - 1, i + 1, j + 1);
+
+    textePage();
+  };
+
+
+
+  if (matrice[i][j] == player &&
+    matrice[i - 1][j - 1] == player &&
+    matrice[i - 2][j - 2] == player &&
+    matrice[i - 3][j - 3] == player) {
+    resultat(i, j, i - 1, j - 1, i - 2, j - 2, i - 3, j - 3);
+    textePage();
+  }
+  else {
+    textePage();
+    console.log("No winner yet!");
+    console.log("row:" + i + " " + "column:" + j);
+  }
+
+}
+
+var columns = ['OUT', 'OUT', 'OUT', 'OUT', 'OUT', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'OUT', 'OUT', 'OUT', 'OUT', 'OUT'];
+var rows = ['OUT', 'OUT', 'OUT', 'OUT', 'OUT', '1', '2', '3', '4', '5', '6', 'OUT', 'OUT', 'OUT', 'OUT', 'OUT'];
+var counters = [0, 0, 0, 0, 0, 0, 0];
+var lettres = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+for (var i = 5; i <= 11; i++) {
+  for (var j = 5; j <= 10; j++) {
+    var id = columns[i] + rows[j];
+    var element = document.getElementById(id);
+    element.addEventListener("click", function () {
+      var column = columns.indexOf(this.id.substring(0, 1));
+      var row = 10 - counters[column - 5];
+      if (row < 5) {
+        return;
+      }
+      console.log(player)
+
+      if (player == "01") {
+        document.getElementById(columns[column] + rows[row]).setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#red");
+      }
+      else if (player == "02") {
+        document.getElementById(columns[column] + rows[row]).setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#yellow");
+      }
+      matriceSplit = matrice[row][column].split("");
+      indiceLetter = lettres.indexOf(matriceSplit[0]);
+      console.log("Column: " + (lettres.indexOf(matriceSplit[0]) + 1));
+
+      console.log("Row: " + matriceSplit[1]);
+      counters[column - 5]++;
+      matrice[row][column] = player;
+
+
+      console.log(matrice);
+      console.log(parseInt(matriceSplit[1]) + 5);
+      console.log(lettres.indexOf(matriceSplit[0]) + 5);
+      leI = parseInt(matriceSplit[1]) + 4;
+      leJ = lettres.indexOf(matriceSplit[0]) + 5;
+      console.log(leJ)
+      console.log(leI)
+
+      checker(leJ, leI, player);
+      if (player == "01") {
+        player = "02";
+      }
+      else {
+        player = "01";
+      }
+
+    });
+  }
+}
